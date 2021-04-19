97476e1295257dca6e0968dc2c3ba6faf77fdfe2
MichaelB21 <72177613+MichaelB21@users.noreply.github.com>
2021-04-19 19:59:00 -0400

    asda

---
+++ main.js
@@ -0,0 +1,93 @@
+(function(){
+    function buildQuiz(){
+      const output = [];
+  
+      myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+          const answers = [];
+
+          for(letter in currentQuestion.answers){
+  
+            answers.push(
+              `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+              </label>`
+            );
+          }
+  
+          output.push(
+            `<div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join('')} </div>`
+          );
+        }
+      );
+
+      quizContainer.innerHTML = output.join('');
+    }
+  
+    function showResults(){
+  
+      const answerContainers = quizContainer.querySelectorAll('.answers');
+      let numCorrect = 0;
+
+      myQuestions.forEach( (currentQuestion, questionNumber) => {
+  
+
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  
+        if(userAnswer === currentQuestion.correctAnswer){
+          numCorrect++;
+  
+    
+          answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+        else{
+          answerContainers[questionNumber].style.color = 'red';
+        }
+      });
+  
+      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    }
+  
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+    const myQuestions = [
+      {
+        question: "Who invented JavaScript?",
+        answers: {
+          a: "Douglas Crockford",
+          b: "Sheryl Sandberg",
+          c: "Brendan Eich"
+        },
+        correctAnswer: "c"
+      },
+      {
+        question: "Which one of these is a JavaScript package manager?",
+        answers: {
+          a: "Node.js",
+          b: "TypeScript",
+          c: "npm"
+        },
+        correctAnswer: "c"
+      },
+      {
+        question: "Which tool can you use to ensure code quality?",
+        answers: {
+          a: "Angular",
+          b: "jQuery",
+          c: "RequireJS",
+          d: "ESLint"
+        },
+        correctAnswer: "d"
+      }
+    ];
+  
+    buildQuiz();
+  
+    submitButton.addEventListener('click', showResults);
+  })();
